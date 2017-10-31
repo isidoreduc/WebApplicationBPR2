e38e37c307156b3cc57f06e8e92b471e396be622
isidore <isidoreducasse79@gmail.com>
2017-10-31 18:29:46 +0100

    implemented first appcontroller with actions and related views.

---
+++ WebApplicationBPR2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplicationBPR2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run(); // starts a web host and starts listening to web requests
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args) // the webhost creates our default builder that we use in main method
+                .UseStartup<Startup>() // and tells it what class to use to set up how to listen to web requests
+                .Build(); // then it builds it so it can run it in main method
+    }
+}
---
+++ WebApplicationBPR2/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplicationBPR2
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) // basically saying when you start up tell me how you wanna listen for web requests, what do you want to do when web requests are executed
+        {
+            app.UseDefaultFiles(); // maps automatically what file to serve from our wwwroot (index.html)
+            app.UseStaticFiles(); // after mapping, now it uses the mapped static files (order matters!!!)
+        }
+    }
+}
